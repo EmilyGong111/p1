fef75c8a6c37b15539998800665dc870b5b5aa63
Emily Gong <mei.gong@icloud.com>
2022-04-07 19:08:45 +1000

    photograper/Add window.onscroll function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
+
 let checkBox=document.getElementById('nav-toggle');
 
 window.onscroll = () =>{
-    
-
-    if(window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
-    }else{
-        document.querySelector('.header').classList.remove('active');
-    }
+    checkBox.checked=false;
 }
